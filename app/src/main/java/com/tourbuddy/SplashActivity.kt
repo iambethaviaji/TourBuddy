621b5274ca791dc60c08f7901f58f392198f2c48
Billie014 <billie.santoso@gmail.com>
2024-05-22 19:11:49 +0700

    Splash Screen

--- app/src/main/java/com/tourbuddy/SplashActivity.kt
+++ app/src/main/java/com/tourbuddy/SplashActivity.kt
@@ -1,11 +1,27 @@
 package com.tourbuddy
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            goToMain()
+        }, 3000L)
+    }
+
+    private fun goToMain(){
+        Intent(this, MainActivity::class.java).also {
+            startActivity(it)
+            finish()
+        }
     }
 }
