450c6c63a51b3070a9643130a5f8cc0c47b47acb
danish <danishalif16@gmail.com>
2024-05-24 09:14:58 +0700

    flow from details to review

--- app/src/main/java/com/tourbuddy/DetailActivity.kt
+++ app/src/main/java/com/tourbuddy/DetailActivity.kt
@@ -1,20 +1,38 @@
 package com.tourbuddy
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.tourbuddy.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragmentManager = supportFragmentManager
+        val detailFragment = DetailFragment()
+        val fragment = fragmentManager.findFragmentByTag(DetailFragment::class.java.simpleName)
+        if (fragment !is DetailFragment) {
+            fragmentManager
+                .beginTransaction()
+                .add(R.id.frame_container, detailFragment, DetailFragment::class.java.simpleName)
+                .commit()
+        }
+
+        binding.buttonBack.setOnClickListener{
+            startActivity(Intent(this@DetailActivity, MainActivity::class.java))
+            finish()
+        }
+
+        //todo bookmark story
+        binding.buttonBookmark.setOnClickListener{
+
         }
     }
 }
